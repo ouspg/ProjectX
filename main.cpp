9514070390d799280c8e32b7180552127e64cc7a
Asad Hasan <113350302+asadhasan73@users.noreply.github.com>
2023-09-06 04:59:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include "src/calculator.h"
 
 int main() {
-    calculator(1000, '*', 3);
-
-    calculator(500, '+', 200);
-
-    calculator(100, '/', 0);
-
-    calculator(1, 'A', 1);
-    calculator(2147483647,'-', 1);
-    calculator(2147483647,'*', 2147483647);
-    calculator(2000000000, '+', 2000000123);
+    // Function calls to calculate
+    // Write your all possible input scenarios here
+    // Notice that calculator code written here works with integers
     return 0;
 }
